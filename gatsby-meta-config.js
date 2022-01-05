cbfef2725ea92817f431befef5aa644d7b831844
atharva20-coder <atharvajoshi2520@gmail.com>
2022-01-05 15:49:47 +0530

    dsa blog 2 and uttrances update

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: `Blog posted about someting hilarious...`,
   author: `[Atharva Joshi]`,
   introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  siteUrl: `https://atharvacodes.netlify.app/`, // Your blog site url
   social: {
     twitter: `ceo_joshi`, // Your Twitter account
     github: `atharva20-coder`, // Your GitHub account
@@ -16,7 +16,7 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'atharva20-coder/AtharvaCodes', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 20, // Config your initial count of post
